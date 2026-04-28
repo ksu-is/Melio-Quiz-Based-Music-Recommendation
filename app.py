4c72b211623872472da1668761f45b96407ac36f
Beza Desta <bdesta3@students.kennesaw.edu>
2026-04-27 23:41:17 -0400

    Add results route to handle quiz submissions

--- app.py
+++ app.py
@@ -1,3 +1,12 @@
+from flask import request
+
 @app.route("/quiz")
 def quiz():
     return render_template("quiz.html")
+
+@app.route("/results", methods=["POST"])
+def results():
+    mood = request.form.get("mood")
+    genre = request.form.get("genre")
+
+    return f"You selected {mood} and {genre}"
